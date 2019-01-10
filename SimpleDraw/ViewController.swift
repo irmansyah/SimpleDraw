1c3a5cfd01dd0d35e7d31cd5bf1c29aeeae52439
Irmansyah <irmansyahproject@gmail.com>
2019-01-10 20:20:41 +0700

    first commit

--- SimpleDraw/ViewController.swift
+++ SimpleDraw/ViewController.swift
@@ -8,11 +8,71 @@
 
 import UIKit
 
+class Canvas: UIView {
+    override func draw(_ rect: CGRect) {
+        // custom drawing
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else{ return }
+        
+        // here are my line
+        // dummy data
+//        let startPoint = CGPoint(x: 0, y: 0)
+//        let endPoint = CGPoint(x: 100, y: 100)
+//
+//        context.move(to: startPoint)
+//        context.addLine(to: endPoint)
+        
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setLineWidth(10)
+        context.setLineCap(.butt)
+        
+        lines.forEach { (line) in
+            for (i, p) in line.enumerated() {
+                if i == 0 {
+                    context.move(to: p)
+                } else {
+                    context.addLine(to: p)
+                }
+            }
+        }
+        
+        context.strokePath()
+    }
+    // let's ditch this line
+//    var line = [CGPoint]()
+    var lines = [[CGPoint]]()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
+    }
+    
+    // track the finger as we move across the line
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point = touches.first?.location(in: nil) else { return }
+        
+        guard var lastLine = lines.popLast() else { return }
+        lastLine.append(point)
+        lines.append(lastLine)
+        
+//        var lastLine = lines.last
+//        lastLine?.append(point)
+        
+//        line.append(point)
+        setNeedsDisplay()
+    }
+}
+
 class ViewController: UIViewController {
 
+    let canvas = Canvas()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(canvas)
+        canvas.backgroundColor = .white
+        canvas.frame = view.frame
     }
 
 
